4cd914d89fb199e779435d152468a98ba44109b9
dstenb <dstenb@gmail.com>
2012-02-09 19:38:24 +0100

    some work done on reading

--- libliveview.c
+++ libliveview.c
@@ -1,5 +1,29 @@
 #include "libliveview.h"
 
+static void send_msg(struct liveview *lv, int id, uint32_t payload_length,
+		char *payload)
+{
+	char header[6];
+
+	header[0] = id;
+	header[1] = 4;
+	header[2] = (payload_length >> 24) & 0xFF;
+	header[3] = (payload_length >> 16) & 0xFF;
+	header[4] = (payload_length >> 8) & 0xFF;
+	header[5] = (payload_length) & 0xFF;
+
+	if (lv->fd != -1) {
+		write(lv->fd, header, 6);
+		write(lv->fd, payload, payload_length);
+	}
+}
+
+static void send_display_properties_request(struct liveview *lv)
+{
+	send_msg(lv, M_DISPLAY_PROPERTIES_REQUEST, strlen(SW_VERSION),
+			SW_VERSION);
+}
+
 sdp_session_t *register_service()
 {
 	uint32_t service_uuid_int[] = { 0, 0, 0, 0xC0FE };
@@ -87,5 +111,101 @@ int liveview_connect(struct liveview *lv)
 	lv->fd = accept(lv->listen_fd, (struct sockaddr *)&addr, &opt);
 	sdp_close(lv->session);
 
+	send_display_properties_request(lv);
+
 	return lv->fd;
+}
+
+int liveview_msg_free(struct liveview_msg *msg)
+{
+	if (msg) {
+		if (msg->payload)
+			free(msg->payload);
+		free(msg);
+	}
+}
+
+int liveview_msg_read(struct liveview *lv,
+		struct liveview_msg *msg)
+{
+	uint32_t payload_read = 0;
+	char buf[4];
+	char *p;
+
+	if (read(lv->fd, msg->id, 1) < 0)
+		return -1;
+	if (read(lv->fd, &msg->header_len, 1) < 0)
+		return -1;
+	if (read(lv->fd, buf, 4) < 0)
+		return -1;
+
+	msg->payload_len = buf[0] << 24 | buf[1] << 16 | buf[2] << 8 | buf[3];
+
+	if (!(msg->payload = calloc(msg->payload_len, sizeof(char))))
+		return -1;
+
+	p = msg->payload;
+	while (msg->payload_len > payload_read) {
+		int32_t tmp = read(lv->fd, p, msg->payload_len - payload_read);
+
+		if (tmp < 0)
+			return -1;
+		payload_read += (uint32_t)tmp;
+		p += tmp;
+	}
+
+	return 0;
+}
+
+int liveview_read(struct liveview *lv, struct liveview_event *ev)
+{
+	struct liveview_msg *msg;
+
+	if (!(msg = calloc(1, sizeof(struct liveview_msg))))
+		return -1;
+
+	if (liveview_msg_read(lv, msg) < 0) {
+		printf("msg_Read\n");
+		liveview_msg_free(msg);
+		return -1;
+	}
+
+	liveview_msg_free(msg);
+
+	return 0;
+#if 0
+
+	char buf[4];
+	char *p;
+
+	printf("read!!!\n");
+	if (read(lv->fd, &msg.id, 1) < 0)
+		pexit("read id");
+	if (read(lv->fd, &msg.header_len, 1) < 0)
+		pexit("header");
+	if (read(lv->fd, buf, 4) < 0)
+		pexit("payload");
+
+	msg.payload_len = buf[0] << 24 | buf[1] << 16 |
+		buf[2] << 8 | buf[3];
+
+	if (!(msg.payload = calloc(msg.payload_len, sizeof(char))))
+		return -1;
+
+	p = msg.payload;
+	while (msg.payload_len > payload_read) {
+		int32_t tmp = read(lv->fd, p, msg.payload_len - payload_read);
+
+		if (tmp < 0)
+			pexit("payload");
+		payload_read += tmp;
+		p += tmp;
+	}
+	printf("id: %i\n", msg.id);
+	printf("header: %i\n", msg.header_len);
+	printf("payload: %u\n", msg.payload_len);
+#endif
+
+
+	return 0;
 }
